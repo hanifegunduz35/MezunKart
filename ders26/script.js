9925e560af3cac2bb033a4f2160b13185368fbc2
hanifegündüz35 <hanifegunduz35@gmail.com>
2026-07-04 12:57:47 +0300

    Backup before responsiveness

---
+++ ders26/script.js
@@ -0,0 +1,32 @@
+// bu bir yorum satırıdır.
+// let x=10
+// var y=45
+// console.log(x,y)
+// const a=90
+// console.log(a)
+// let isim="emre"
+// console.log(isim)
+
+//değişken tanımlarken dolar alt çizgi veya yazıyla başlamak zorundadır.
+// 3 tip değişken tanımlama şekli vardır
+// 1 let moders js te genellikle kullanılan tanımlama türüdür.
+// 2 var eskidir günümüzde çok kullanılmaz.
+// 3 const içeriği değiştirilemez sabit veriler için kullanılır.
+
+// let Isim="deneme"
+// console.log(ISim)
+//js büyük küçük harfe duyarlıdır
+
+// console.log("Hello World")  //console.log consol da çıktıları verir
+// alert("Hello World") // uyarı gibi sayfa ilk yüklendiğinde verir
+// document.write("Hello World") // doğru değildir sayfada sayfa çıktısı vardır
+
+// let isim=prompt("lütfen bir isim giriniz")
+// document.write(isim)
+
+let isim=prompt("lütfen bir isim giriniz")
+let soyisim=prompt("lütfen bir isim giriniz")
+let mrb="Merhaba"
+let hg="hoşgeldiniz."
+// alert(mrb+" "+isim+" "+soyisim+" "+hg)
+alert(`${isim} ${soyisim} aramıza hoşgeldiniz`)
